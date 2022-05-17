ebd032f73193ddbc8c65d427fd222352217930e4
Jeff Sani <jeff.sani@gmail.com>
2022-05-17 15:27:54 -0700

    added path

--- scripts/nsc2e.sh
+++ scripts/nsc2e.sh
@@ -13,7 +13,7 @@ for file in *.tar.gz; do tar -xzf "$file" --directory nsc2e-tmp; done
 find nsc2e-tmp/newnslog* -prune -type d | while IFS= read -r d; do 
    #Process the counter data for nCores
    for f in $d/*; do 
-   nsc2e -c /netscaler/nsconmsg -K "$f" -f nsc2e.conf;
+   ./nsc2e -c /netscaler/nsconmsg -K "$f" -f nsc2e.conf;
    done
    #Concatenate the resultant files
    awk 'FNR==1 && NR!=1 { while (/^"UTC"/) getline; } 1 {print}' nsc2e-nsc2e.conf-newnslog.ppe.* > $d.txt;
