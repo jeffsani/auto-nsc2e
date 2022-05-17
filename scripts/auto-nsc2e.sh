234062e75a6b8a0a9ddca763a47f9faad5b5fc14
Jeff Sani <jeff.sani@gmail.com>
2022-05-17 16:00:34 -0700

    cleaned up syntax

--- scripts/auto-nsc2e.sh
+++ scripts/auto-nsc2e.sh
@@ -12,13 +12,13 @@ CITRIX_ADC_PASSWORD="Marig0ld"
 
 #Cleanup function
 function do_cleanup {
-echo "Searching for old logs > 30 days and removing them..." | ts '[%H:%M:%S]' | tee -a $LOGFILE;
+echo "Searching for old logs > 30 days and removing them..." | ts '[%H:%M:%S]' | tee -a $LOGFILE
 find *.log -type f -not -name '*auto-nsc2e-init.log' -mtime -30 -delete
 }
 
 #Start Logging
-echo "User $(whoami) started the script" | ts '[%H:%M:%S]' | tee -a $LOGFILE;
-echo "Starting auto-nsc2e Log..." | ts '[%H:%M:%S]' | tee -a $LOGFILE;
+echo "User $(whoami) started the script" | ts '[%H:%M:%S]' | tee -a $LOGFILE
+echo "Starting auto-nsc2e Log..." | ts '[%H:%M:%S]' | tee -a $LOGFILE
 
 # Check to see if one of the required environment variables for the script is not set
 source ~/.bashrc
@@ -35,17 +35,17 @@ do
   echo "Now processing ADC at $CITRIX_ADC_IP on Port: $CITRIX_ADC_PORT..." | ts '[%H:%M:%S]' | tee -a $LOGFILE;
   #Transfer tool and configuration to ADC
   echo "Transfering files to ADC..." | ts '[%H:%M:%S]' | tee -a $LOGFILE;
-  sshpass -p "$CITRIX_ADC_PASSWORD" scp -q -P $CITRIX_ADC_PORT ../nsc2e/nsc2e ../nsc2e/nsc2e.conf nsc2e.sh $CITRIX_ADC_USER@$CITRIX_ADC_IP:$NEWNSLOG_PATH; >> $LOGFILE
-  echo "Setting execute permissions on nsc2e..." | ts '[%H:%M:%S]' | tee -a $LOGFILE;
-  sshpass -p "$CITRIX_ADC_PASSWORD" ssh -q $CITRIX_ADC_USER@$CITRIX_ADC_IP -p $CITRIX_ADC_PORT "shell chmod 744 $NEWNSLOG_PATH/nsc2e.sh $NEWNSLOG_PATH/nsc2e"; >> $LOGFILE
-  echo "Executing nsc2e remotely..." | ts '[%H:%M:%S]' | tee -a $LOGFILE;
+  sshpass -p "$CITRIX_ADC_PASSWORD" scp -q -P $CITRIX_ADC_PORT ../nsc2e/nsc2e ../nsc2e/nsc2e.conf nsc2e.sh $CITRIX_ADC_USER@$CITRIX_ADC_IP:$NEWNSLOG_PATH >> $LOGFILE
+  echo "Setting execute permissions on nsc2e..." | ts '[%H:%M:%S]' | tee -a $LOGFILE
+  sshpass -p "$CITRIX_ADC_PASSWORD" ssh -q $CITRIX_ADC_USER@$CITRIX_ADC_IP -p $CITRIX_ADC_PORT "shell chmod 744 $NEWNSLOG_PATH/nsc2e.sh $NEWNSLOG_PATH/nsc2e" >> $LOGFILE
+  echo "Executing nsc2e remotely..." | ts '[%H:%M:%S]' | tee -a $LOGFILE
   sshpass -p "$CITRIX_ADC_PASSWORD" ssh -q $CITRIX_ADC_USER@$CITRIX_ADC_IP -p $CITRIX_ADC_PORT "shell /bin/sh $NEWNSLOG_PATH/nsc2e.sh"; >> $LOGFILE
-  echo "Transferring data back to script host..." | ts '[%H:%M:%S]' | tee -a $LOGFILE;
-  sshpass -p "$CITRIX_ADC_PASSWORD" scp -q -P $CITRIX_ADC_PORT $CITRIX_ADC_USER@$CITRIX_ADC_IP:$NEWNSLOG_PATH/nsc2e.txt ./$(date '+%m%d%Y')-$CITRIX_ADC_IP.txt; >> $LOGFILE
-  echo "Removing remote files and folders..." | ts '[%H:%M:%S]' | tee -a $LOGFILE;
-  sshpass -p "$CITRIX_ADC_PASSWORD" ssh -q $CITRIX_ADC_USER@$CITRIX_ADC_IP -p $CITRIX_ADC_PORT "shell rm -rf $NEWNSLOG_PATH/nsc2e*"; >> $LOGFILE
-  echo "Done processing $CITRIX_ADC_IP..." | ts '[%H:%M:%S]' | tee -a $LOGFILE;
+  echo "Transferring data back to script host..." | ts '[%H:%M:%S]' | tee -a $LOGFILE
+  sshpass -p "$CITRIX_ADC_PASSWORD" scp -q -P $CITRIX_ADC_PORT $CITRIX_ADC_USER@$CITRIX_ADC_IP:$NEWNSLOG_PATH/nsc2e.txt ./$(date '+%m%d%Y')-$CITRIX_ADC_IP.txt >> $LOGFILE
+  echo "Removing remote files and folders..." | ts '[%H:%M:%S]' | tee -a $LOGFILE
+  sshpass -p "$CITRIX_ADC_PASSWORD" ssh -q $CITRIX_ADC_USER@$CITRIX_ADC_IP -p $CITRIX_ADC_PORT "shell rm -rf $NEWNSLOG_PATH/nsc2e*" >> $LOGFILE
+  echo "Done processing $CITRIX_ADC_IP..." | ts '[%H:%M:%S]' | tee -a $LOGFILE
 done < $INPUT
-echo "All done..." | ts '[%H:%M:%S]' | tee -a $LOGFILE;
+echo "All done..." | ts '[%H:%M:%S]' | tee -a $LOGFILE
 
 do_cleanup
--- scripts/nsc2e.sh
+++ scripts/nsc2e.sh
@@ -13,13 +13,13 @@ for file in *.tar.gz; do tar -xzf "$file" --directory nsc2e-tmp; done
 find nsc2e-tmp/newnslog* -prune -type d | while IFS= read -r d; do 
    #Process the counter data for nCores
    for f in $d/*; do 
-   ./nsc2e -c /netscaler/nsconmsg -K "$f" -f nsc2e.conf;
+   ./nsc2e -c /netscaler/nsconmsg -K "$f" -f nsc2e.conf
    done
    #Concatenate the resultant files
-   awk 'FNR==1 && NR!=1 { while (/^"UTC"/) getline; } 1 {print}' nsc2e-nsc2e.conf-newnslog.ppe.* > $d.txt;
+   awk 'FNR==1 && NR!=1 { while (/^"UTC"/) getline; } 1 {print}' nsc2e-nsc2e.conf-newnslog.ppe.* > $d.txt
    #Remove ppe files
    rm nsc2e-nsc2e.conf-newnslog.ppe.*
 done
 #Concatenate all data to single file
-awk 'FNR==1 && NR!=1 { while (/^"UTC"/) getline; } 1 {print}' nsc2e-tmp/*.txt > nsc2e.txt;
+awk 'FNR==1 && NR!=1 { while (/^"UTC"/) getline; } 1 {print}' nsc2e-tmp/*.txt > nsc2e.txt
 exit 0
