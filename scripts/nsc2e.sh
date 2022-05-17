be5a1ccb4f46203ce5131cd9642c05958ed9fc41
Jeff Sani <jeff.sani@gmail.com>
2022-05-17 10:42:59 -0700

    updated remote script

--- scripts/nsc2e.sh
+++ scripts/nsc2e.sh
@@ -1,22 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 #nsc2e.sh
 #This script initiates the processng of the nsc2e utility against all newnslog archives
 
 set -o pipefail
 
 #Create a temporary directory for the xfer
-mkdir nsc2e
+mkdir nsc2e-tmp
 #Untar all newnslog files
-for file in *.tar.gz; do tar -xzf "nsc2e/$(file)"; done
+for file in *.tar.gz; do tar -xzf "$file" --directory nsc2e-tmp; done
 #shopt -s dotglob
-find /nsc2e/newnslog* -prune -type d | while IFS= read -r d; do 
-    #Process the counter data for nCores
-    for f in $d/*; do ../../nsc2e -c /netscaler/nsconmsg -K "$d/$f" -f ../../nsc2e.conf; done
-    #Concatenate the resultant files
-    awk 'FNR==1 && NR!=1 { while (/^"UTC"/) getline; } 1 {print}' nsc2e-nsc2e.conf-newnslog.ppe.* >$(f).txt;
-    #Move data file to nsc2e dir
-    mv $(f) ../nsc2e
+find nsc2e-tmp/newnslog* -prune -type d | while IFS= read -r d; do 
+   #Process the counter data for nCores
+   for f in $d/*; do 
+   nsc2e -c /netscaler/nsconmsg -K "$f" -f nsc2e.conf;
+   done
+   #Concatenate the resultant files
+   awk 'FNR==1 && NR!=1 { while (/^"UTC"/) getline; } 1 {print}' nsc2e-nsc2e.conf-newnslog.ppe.* > $d.txt;
+   #Remove ppe files
+   rm nsc2e-nsc2e.conf-newnslog.ppe.*
 done
 #Concatenate all data to single file
-awk 'FNR==1 && NR!=1 { while (/^"UTC"/) getline; } 1 {print}' *.txt >nsc2e.txt;
+awk 'FNR==1 && NR!=1 { while (/^"UTC"/) getline; } 1 {print}' nsc2e-tmp/*.txt > nsc2e.txt;
 exit 0
