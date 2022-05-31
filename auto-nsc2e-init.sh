cd0c8d0b6cdf54a0163d16f67013be1a71949e73
Jeff Sani <jeff.sani@gmail.com>
2022-05-31 13:32:46 -0700

    trying something else...

--- auto-nsc2e-init.sh
+++ auto-nsc2e-init.sh
@@ -14,9 +14,8 @@ echo "checking for log and data directories and creating if they do not exist...
 
 # Prompt for and set rc variables 
 echo "Setting script variables in ~/.bashrc..."
-echo "Enter the Citrix ADC user for the script: "
-read ADC_USER
-read -s -p "Enter the Citrix ADC user password: " ADC_PASSWD
+echo "Enter the Citrix ADC user for the script: "; read ADC_USER
+echo "Enter the Citrix ADC user password: "; read -s ADC_PASSWD
 echo ""
 if [ ! -z "$NSC2E_ADC_USER" ] && [ ! -z "$NSC2E_ADC_PASSWORD" ] && [ ! -z "$SSHPASS" ]; then
    sed -i -e "s/NSC2E_ADC_USER=.*/NSC2E_ADC_USER=$ADC_USER/" -e "s/NSC2E_ADC_PASSWORD=.*/NSC2E_ADC_PASSWORD=\'$ADC_PASSWD\'/" -e "s/SSHPASS=.*/SSHPASS=\'$ADC_PASSWD\'/" ~/.bashrc
@@ -40,8 +39,7 @@ fi
 echo "Script variables set successfully..."
 
 # Download and install pre-requisites
-echo "Do you want to install required system pre-requisites (requires elevated privs or sudoer membership) [Y/n]? " 
-read ANSWER1
+echo "Do you want to install required system pre-requisites (requires elevated privs or sudoer membership) [Y/n]? "; read ANSWER1
 ANSWER1=${ANSWER1,,} # convert to lowercase
 if [ "$ANSWER1" == "y" ]; then
    echo "Installing required system pre-requisites..."
@@ -85,21 +83,21 @@ if [ $(grep -cE "[0-9][0-9]*.[0-9][0-9]*\.[0-9][0-9]*.[0-9][0-9]*:[0-9][0-9]*" $
 else
    #Prompt for first ADC IP and Port to write to adc-list.txt
    echo "No entries found in adc-list.txt - at least one host is required to run the script..."
-   read -p "Input an ADC IP with management enabled - either NSIP or SNIP: " ANSWER2
-   read -p "Input the ADC Port: " ANSWER3
+   echo "Input an ADC IP with management enabled - either NSIP or SNIP: "; read ANSWER2
+   echo "Input the ADC Port: "; read ANSWER3
    echo "$ANSWER2:$ANSWER3" > adc-list.txt
    echo "ADC $ANSWER2:$ANSWER3 added as first entry into adc-list.txt - add any additional ADC hosts in the format X.X.X.X:NN..."
 fi
 
 # Prompt user to create cron job for scheduling the script to be run at a desired interval
-read -p "Would you like to schedule this script to be run - [Y/n]? " ANSWER4
+echo "Would you like to schedule this script to be run - [Y/n]? "; read ANSWER4
 ANSWER4=${ANSWER4,,} # convert to lowercase
 if [ "$ANSWER4" == "y" ]; then
    echo "Removing old cronjob if it exists..."
    crontab -l | grep -v "auto-nsc2e.sh" | crontab -
    echo "Backing up existing entries..."
    crontab -l > auto-nsc2e
-   read -p "What interval would you like to run the script - [Daily/Weekly/Monthly]? " ANSWER5
+   echo "What interval would you like to run the script - [Daily/Weekly/Monthly]? "; read ANSWER5
    ANSWER5=${ANSWER5:0:1} # get first letter
    case $ANSWER5 in
 	d)
