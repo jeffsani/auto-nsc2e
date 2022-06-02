0a269cbbc3e4ae1d8a83594edd254f12f152ec12
Jeff Sani <jeff.sani@gmail.com>
2022-06-01 18:02:30 -0700

    load ~/.bashrc

--- auto-nsc2e.sh
+++ auto-nsc2e.sh
@@ -24,6 +24,7 @@ echo "User $(whoami) started the script"
 echo "Starting auto-nsc2e Log..."
 
 # Check to see if one of the required environment variables for the script is not set
+~/.bashrc
 if [[ -z "$NSC2E_ADC_USER" || -z "$NSC2E_ADC_PASSWORD" || -z "$SSHPASS" ]]; then
     echo "One or more of the required environment variables for the script is not set properly..."
     exit 1;
