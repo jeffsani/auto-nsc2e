8ea37eaba88b62a3f3177c0dc9fb502cf271e889
Jeff Sani <jeff.sani@gmail.com>
2022-07-01 16:24:38 -0700

    improved the logic to optimize space consumption on ADC

--- scripts/nsc2e.sh
+++ scripts/nsc2e.sh
@@ -12,20 +12,28 @@ cd /var/tmp/nsc2e-tmp
 cp -r /var/nslog/newnslog .
 
 #Untar all newnslog archives
-for file in /var/nslog/*.tar.gz; do tar -xzf "$file"; done
-find newnslog* -prune -type d | while IFS= read -r d; do 
-   #Process the counter data for nCores
-   for f in $d/*; do 
-   ./nsc2e -c /netscaler/nsconmsg -K "$f" -f nsc2e.conf
+for file in /var/nslog/*.tar.gz; do tar -xzf "$file"
+   find newnslog* -prune -type d | while IFS= read -r d; do 
+      #Process the counter data for nCores
+      for f in $d/*; do 
+         ./nsc2e -c /netscaler/nsconmsg -K "$f" -f nsc2e.conf
+      done
+      #Concatenate the resultant files
+      #head -n +1 nsc2e-nsc2e.conf-newnslog.ppe.0 >$d.tsv
+      #tail -q -n +2 nsc2e-nsc2e.conf-newnslog.ppe.* >>$d.tsv
+      awk 'FNR==1 && NR!=1 { while (/^"UTC"/) getline; } 1 {print}' nsc2e-nsc2e.conf-newnslog.ppe.* >$d.tsv
+      #Remove ppe files
+      rm nsc2e-nsc2e.conf-newnslog.ppe.*
    done
-   #Concatenate the resultant files
-   awk 'FNR==1 && NR!=1 { while (/^"UTC"/) getline; } 1 {print}' nsc2e-nsc2e.conf-newnslog.ppe.* > $d.tsv
-   #Remove ppe files
-   rm nsc2e-nsc2e.conf-newnslog.ppe.*
+   #Remove processed newnslog file to optimize disk space usage
+   rm "$file"
 done
 
 #Concatenate all data to single file
-awk 'FNR==1 && NR!=1 { while (/^"UTC"/) getline; } 1 {print}' *.tsv > nsc2e.tsv
+awk 'FNR==1 && NR!=1 { while (/^"UTC"/) getline; } 1 {print}' *.tsv >nsc2e.tsv
 
 #Compress file for transfer to script host
 tar -czf nsc2e.tsv.gz nsc2e.tsv
+
+#Remove aggregate newnslog to optimize disk space usage
+rm nsc2e.tsv 
